69596b74c30518461e466f159281d7662d648881
Chiranth R Raichur <chiranth.raichur@gmail.com>
2026-01-01 14:51:58 +0530

    Need of JSX

--- App.js
+++ App.js
@@ -2,7 +2,11 @@
 //Create a Nested elements using React
 
   <div id="parent">
-        <div id="child">
+        <div id="child1">
+            <h1>Iam H1 Tag</h1>
+            <h1> Iam H2 Tag </h1>
+        </div>
+         <div id="child2">
             <h1>Iam H1 Tag</h1>
             <h1> Iam H2 Tag </h1>
         </div>
@@ -12,12 +16,20 @@
 const parent = React.createElement(
     "div", 
     { id: "parent" }, 
-    React.createElement(
+   [ React.createElement(
         'div', 
-        { id: "child" }, 
+        { id: "child1" }, 
+        [React.createElement('h1', {}, "Iam H1 Tag"), React.createElement('h1', {}, "Iam H2 Tag")]
+    ),
+     React.createElement(
+        'div', 
+        { id: "child2" }, 
         [React.createElement('h1', {}, "Iam H1 Tag"), React.createElement('h1', {}, "Iam H2 Tag")]
     )
+    ]
 );
+console.log(parent);
+
 const root = ReactDOM.createRoot(document.getElementById('head'));
 
 
