cd518e7d48a5f56b19d7fd661db68ab86acaf511
Chiranth R Raichur <chiranth.raichur@gmail.com>
2026-01-04 15:04:10 +0530

    Component Composition & { }

--- App.js
+++ App.js
@@ -2,29 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 //React Element (i.e. Object)
-const headingJSX = <h1 id="crr" className="crr_1"> CRR_React </h1>;
+//const headingJSX = <h1 id="crr" className="crr_1"> CRR_React </h1>;
 
-//React Components:
-// 1. Functional Component -> latest way to write React components
-// 2. Class-based Component => old way
 
-//Functional Component
+const Title = () => <h1>This is CRR Adda</h1> // No need to use return since it is a single line. It is Implicit return
+
+const TitleSub = () => (            // instead of return use () for returning compo
+    <>              
+    <h2>This is CRR Adda Sub Title</h2>
+    <h2>{1000}</h2>
+    <h3> Be Chill </h3>
+    </>   
+);
+
+//Component Composition
 const HeadingFunctionalComponent = () => {
-    return <h1 id="crr" className="crr_1"> CRR_React </h1>;
+    return <div id="Container">
+        {Title()}
+        <TitleSub />
+        <h2 id="crr" className="crr_1"> CRR_React </h2>
+    </div>
 }
 
-// Class-based Component (old way)
-class HeadingClassComponent extends React.Component {
-  render() {
-    return (
-      <h1 id="crr" className="crr_1">
-        CRR_React
-      </h1>
-    );
-  }
-}
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<HeadingFunctionalComponent />);
-//root.render(<HeadingClassComponent />);
